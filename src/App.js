22d293dc31bb8354b9af36d9039f796a807af988
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-08-14 00:43:51 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Game from './Game';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
 // Add this in your component file
 require('react-dom');
 window.React2 = require('react');
 console.log(window.React1 === window.React2);
 function App() {
   return (
-    <div className="App">
-      <Game/>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+          <Route component={Game} path="/" exact/>
+      </div>
+  </BrowserRouter>
+
   );
 }
 
